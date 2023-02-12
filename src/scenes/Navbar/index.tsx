3d1ba5edfeed7e9a8ed5d5597c2aa147385d6af5
Yasindu Sanjeewa <yasindusanjeewa8@gmail.com>
2023-02-12 16:03:30 +0530

    First commit

---
+++ src/scenes/Navbar/index.tsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+import {Bars3Icon, XMarkIcon} from "@heroicons/react/24/solid";
+
+type Props = {}
+
+const Navbar = (props: Props) => {
+  const flexBetween = "flex items-center justify-between"; 
+    
+  return (
+    <nav>
+
+    </nav>
+  )
+};
+
+export default Navbar;
