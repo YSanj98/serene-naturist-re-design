0e7aa1997015d7f26f2d70f55dddd33a8faabaca
Yasindu Sanjeewa <yasindusanjeewa8@gmail.com>
2023-02-13 20:02:09 +0530

    add mediaQuery hook

--- src/scenes/Navbar/Link.tsx
+++ src/scenes/Navbar/Link.tsx
@@ -15,9 +15,9 @@ const Link = ({
     const lowerCasePage = page.toLowerCase().replace(/ /g,'');
 
     return(
-        <AnchorLink className={`${selectedPage === lowerCasePage ? "text-primary-500" : ""}
-        transition duration-500 hover:text-primary-300
-      `} href={`#${lowerCasePage}`} onClick={() => setSelectedPage(lowerCasePage)}>
+        <AnchorLink className={`${selectedPage === lowerCasePage ? "text-yellow-500" : ""} transition duration-500 hover:text-red-500`} 
+                    href={`#${lowerCasePage}`} 
+                    onClick={() => setSelectedPage(lowerCasePage)}>
             {page}
         </AnchorLink>
     );
