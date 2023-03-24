521b073b572be87823dbd533aa41d4a175bd84d4
Yasindu Sanjeewa <yasindusanjeewa8@gmail.com>
2023-03-24 22:34:40 +0530

    AboutUs component created

---
+++ src/components/About us/index.tsx
@@ -0,0 +1,17 @@
+import { SelectedPage } from "@/shared/types";
+
+
+type Props = {
+    setSelectedPage: (Value: SelectedPage) => void;
+}
+
+const AboutUs = (props: Props) => {
+  return (
+    <div>
+        About Us
+    </div>
+
+  )
+}
+
+export default AboutUs
