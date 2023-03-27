3ed3b5c21d7ddef0e51dcf8b8d7fe20e9ea322c7
Yasindu Sanjeewa <yasindusanjeewa8@gmail.com>
2023-03-27 20:04:57 +0530

    change design of service 2

--- src/components/Services/index.tsx
+++ src/components/Services/index.tsx
@@ -131,6 +131,44 @@ const Service = (setSelectedPage: Props) => {
 
           </div>
 
+          <div className="flex justify-around">
+            <div className="text-[#ffffff] card w-96 bg-base-100 bg-primary-100 mb-5">
+              <figure className="px-10 pt-10">
+                <img src="src/assets/images/img-shaving.jpeg" alt="Shaving image" className="rounded-xl" />
+              </figure>
+              <div className="card-body items-center text-center">
+                <h2 className="card-title">Shaving</h2>
+                <p className="text-justify">Ladies and gentleman have
+                  trimmed and pruned and shaped and shaved
+                  all body hair are for ages
+                  but this is where the naturist comes into his/her own.<br />
+                  once you start to shave it seems you can't stop
+                  Be careful, before you know where you are you may
+                  have turned into a smoothie!
+                </p>
+              </div>
+            </div>
+          
+            <div className="text-[#ffffff] card w-96 bg-base-100 bg-primary-100">
+              <figure className="px-10 pt-10">
+                <img src="src/assets/images/img-couple.jpg" alt="Couple image" className="rounded-xl" />
+              </figure>
+              <div className="card-body items-center text-center">
+                <h2 className="card-title">Couple</h2>
+                <p className="text-justify">A naturist massage together can be a
+                  wonderful way to affirm your closeness,
+                  or to rekindle a romantic flame by
+                  taking the time out to do some thing lovely
+                  and relaxing together. it is a special and
+                  a memorable way to celebrate a special occasion. <br />
+                  i often see my self as a catalyst a
+                  sort of a cupid figure, allowing and showing a couple
+                  how to massage, how to nature and be tender with each other
+                </p>
+              </div>
+            </div>
+          </div>
+
           </div>
           )}
     </div>
