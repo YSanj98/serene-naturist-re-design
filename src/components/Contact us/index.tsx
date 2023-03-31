1a964df14f21e562d3735f01bc95615980812fa5
Yasindu Sanjeewa <yasindusanjeewa8@gmail.com>
2023-03-31 14:18:13 +0530

    create ContactUs component

---
+++ src/components/Contact us/index.tsx
@@ -0,0 +1,16 @@
+import { SelectedPage } from "@/shared/types";
+
+
+type Props = {
+    setSelectedPage: (Value: SelectedPage) => void;
+}
+
+const ContactUs = (props: Props) => {
+  return (
+    <div>
+      Contact us
+    </div>
+  )
+}
+
+export default ContactUs
