c2e16f8c8b0142e8d29e1aa7bc7d8924d50cf6d4
Yasindu Sanjeewa <yasindusanjeewa8@gmail.com>
2023-04-05 22:32:01 +0530

    price component

---
+++ src/components/Price/index.tsx
@@ -0,0 +1,17 @@
+import { SelectedPage } from "@/shared/types";
+
+
+type Props = {
+    setSelectedPage: (Value: SelectedPage) => void;
+}
+
+const Price = (props: Props) => {
+  return (
+    <div>
+        About Us
+    </div>
+
+  )
+}
+
+export default Price
