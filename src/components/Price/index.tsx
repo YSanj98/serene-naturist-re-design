3f86a5f6ac43f818867a06b1d071b284f90656bc
Yasindu Sanjeewa <yasindusanjeewa8@gmail.com>
2023-04-09 12:00:37 +0530

    carousel

--- src/components/Price/index.tsx
+++ src/components/Price/index.tsx
@@ -7,9 +7,44 @@ type Props = {
 
 const Price = (props: Props) => {
   return (
-    <div>
-        About Us
+    <div className="flex justify-center myclip bg-[#f9f9f1] pb-[400px] mt-20">
+      <div className="carousel w-3/5 h-50 mt-20">
+
+        <div id="slide1" className="carousel-item relative w-full">
+          <img src="src/assets/images/img1-carousel.jpg" className="w-full" />
+          <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+            <a href="#slide4" className="btn btn-circle">❮</a> 
+            <a href="#slide2" className="btn btn-circle">❯</a>
+          </div>
+        </div> 
+
+        <div id="slide2" className="carousel-item relative w-full">
+          <img src="src/assets/images/img2-carousel.jpg" className="w-full" />
+          <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+            <a href="#slide1" className="btn btn-circle">❮</a> 
+            <a href="#slide3" className="btn btn-circle">❯</a>
+          </div>
+        </div> 
+
+        <div id="slide3" className="carousel-item relative w-full">
+          <img src="src/assets/images/img3-carousel.jpg" className="w-full" />
+          <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+            <a href="#slide2" className="btn btn-circle">❮</a> 
+            <a href="#slide4" className="btn btn-circle">❯</a>
+          </div>
+        </div> 
+
+        <div id="slide4" className="carousel-item relative w-full">
+          <img src="src/assets/images/img4-carousel.jpg" className="w-full" />
+          <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+            <a href="#slide3" className="btn btn-circle">❮</a> 
+            <a href="#slide1" className="btn btn-circle">❯</a>
+          </div>
+        </div>
+
     </div>
+
+</div>
 
   )
 }
