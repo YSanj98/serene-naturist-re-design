3c697afd85c57f84551c29e85f80278d6e73d774
Yasindu Sanjeewa <yasindusanjeewa8@gmail.com>
2023-04-21 14:23:31 +0530

    servicelist component

---
+++ src/components/ServiceList/index.tsx
@@ -0,0 +1,21 @@
+type serviceLisrProps = {
+    image: string
+    serviceTitle: string,
+    description: string
+}
+
+const ServiceList = (props: serviceLisrProps) => {
+  return (
+    <div className="text-[#ffffff] card w-96 bg-primary-100 mb-5">
+        <figure className="px-10 pt-10">
+            <img src={props.image} alt="Shaving image" className="rounded-xl" />
+        </figure>
+        <div className="card-body items-center text-center">
+            <h2 className="card-title">{props.serviceTitle}</h2>
+            <p className="text-justify">{props.description}</p>
+        </div>
+    </div>
+  )
+}
+
+export default ServiceList
