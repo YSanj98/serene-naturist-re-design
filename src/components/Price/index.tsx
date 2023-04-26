3999607044b09ffde5bc05c0286e5d37a7f019a8
Yasindu Sanjeewa <yasindusanjeewa8@gmail.com>
2023-04-26 11:23:17 +0530

    import images

--- src/components/Price/index.tsx
+++ src/components/Price/index.tsx
@@ -1,20 +1,24 @@
 import { SelectedPage } from "@/shared/types";
 import PriceList from "../PriceList";
 import priceData from "@/shared/priceData";
-
+import img1carousel from "@/assets/images/img1-carousel.jpg"
+import img2carousel from "@/assets/images/img2-carousel.jpg"
+import img3carousel from "@/assets/images/img3-carousel.jpg"
+import img4carousel from "@/assets/images/img4-carousel.jpg"
 
 type Props = {
     setSelectedPage: (Value: SelectedPage) => void;
 }
 
+
 const Price = (props: Props) => {
   return (
     <>
-      <div className="flex justify-center bg-[#f9f9f1] pb-[50px] mt-20 md:hidden">
+      <div className="flex justify-center bg-[#f9f9f1] pb-[50px] mt-20">
         <div className="carousel w-3/5 h-50 mt-20">
 
           <div id="slide1" className="carousel-item relative w-full">
-            <img src="src/assets/images/img1-carousel.jpg" className="w-full" />
+            <img src={img1carousel} className="w-full" />
             <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
               <a href="#slide4" className="btn btn-circle">❮</a>
               <a href="#slide2" className="btn btn-circle">❯</a>
@@ -22,7 +26,7 @@ const Price = (props: Props) => {
           </div>
 
           <div id="slide2" className="carousel-item relative w-full">
-            <img src="src/assets/images/img2-carousel.jpg" className="w-full" />
+            <img src={img2carousel} className="w-full" />
             <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
               <a href="#slide1" className="btn btn-circle">❮</a>
               <a href="#slide3" className="btn btn-circle">❯</a>
@@ -30,7 +34,7 @@ const Price = (props: Props) => {
           </div>
 
           <div id="slide3" className="carousel-item relative w-full">
-            <img src="src/assets/images/img3-carousel.jpg" className="w-full" />
+            <img src={img3carousel} className="w-full" />
             <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
               <a href="#slide2" className="btn btn-circle">❮</a>
               <a href="#slide4" className="btn btn-circle">❯</a>
@@ -38,7 +42,7 @@ const Price = (props: Props) => {
           </div>
 
           <div id="slide4" className="carousel-item relative w-full">
-            <img src="src/assets/images/img4-carousel.jpg" className="w-full" />
+            <img src={img4carousel} className="w-full" />
             <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
               <a href="#slide3" className="btn btn-circle">❮</a>
               <a href="#slide1" className="btn btn-circle">❯</a>
