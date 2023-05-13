1a4f9c8eb680bef95d34fefce1c5b93448fc4d7d
Yasindu Sanjeewa <yasindusanjeewa8@gmail.com>
2023-05-13 21:20:19 +0530

    form implementation

--- src/components/Contact us/index.tsx
+++ src/components/Contact us/index.tsx
@@ -1,14 +1,28 @@
-import { SelectedPage } from "@/shared/types";
+import Form from "../Form";
+import inputs from "@/shared/formData";
 
 
-type Props = {
-    setSelectedPage: (Value: SelectedPage) => void;
-}
-
-const ContactUs = (props: Props) => {
   return (
-    <div>
-      Contact us
+    <div className="flex items-center justify-center w-ful mb-10">
+      <div className="flex items-center justify-center bg-gradient-to-r from-[#a8a8b8] via-[#c6c6c6] to-[#a8a8b8] w-3/5 rounded-lg">
+        <div className="flex items-center justify-center">
+          <form action="#" method="POST" onSubmit={handleSubmit} className="">
+            <h1 className="text-center text-[30px] text-primary-100 font-semibold mt-10 mb-10">BOOK AN APPOINMENT</h1>
+            {inputs?.map((input) => (
+              <Form 
+                key={input.id}
+                {...input}
+                value={input.name}
+                onChange={onChange}
+              />
+            ))}
+            <div className="flex items-center justify-center mb-4">
+              <button className="btn transition ease-in-out delay-150 bg-primary-100 hover:-translate-y-1 hover:scale-110 hover:bg-[#f39d1a] duration-300">Submit</button>
+            </div>
+
+          </form>
+        </div>
+      </div>
     </div>
   )
 }
